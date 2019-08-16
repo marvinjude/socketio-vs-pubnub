c5900d8db51d93ccd23fe0bbe8f0cf9d4e77f6d4
Agboola Jude <jude.agboola@charisol.io>
2019-08-16 09:16:57 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
-const chromeLauncher = require('chrome-launcher');
-const puppeteer = require('puppeteer');
-const lighthouse = require('lighthouse');
-const request = require('request');
 const express = require('express');
-const util = require('util');
+const cors = require('cors');
+
+const {
+  asyncMiddleware,
+  extractResult,
+  launchChromeAndRunLighthouse
+} = require('./libs');
 
 const app = express();
-
-const asyncMiddleware = fn => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-async function launchChromeAndRunLighthouse() {
-  const URL = 'https://www.chromestatus.com/features';
-
-  const opts = {
-    chromeFlags: ['--headless'],
-    logLevel: 'info',
-    output: 'json'
-  };
-
-  // Launch chrome using chrome-launcher.
-  const chrome = await chromeLauncher.launch(opts);
-  opts.port = chrome.port;
-
-  // Connect to it using puppeteer.connect().
-  const resp = await util.promisify(request)(
-    `http://localhost:${opts.port}/json/version`
-  );
-  const { webSocketDebuggerUrl } = JSON.parse(resp.body);
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: webSocketDebuggerUrl
-  });
-
-  // Run Lighthouse.
-  return lighthouse(URL, opts, null).then(async result => {
-    await browser.disconnect();
-    await chrome.kill();
-    return result.lhr;
-  });
-}
+app.use(cors());
 
 app.get(
-  '/',
+  '/audit',
   asyncMiddleware(async (req, res, next) => {
-    const results = await launchChromeAndRunLighthouse('https://google.com');
-    res.json(results);
+    if (req.query.url) {
+      const results = await launchChromeAndRunLighthouse(req.query.url);
+      res.json(extractResult(results));
+    } else {
+      res.json({
+        error: true,
+        message: `You're not specifying the URL`
+      });
+    }
   })
 );
 
